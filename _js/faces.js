1dca23b31b97bac211a46360fe43cf1981d756d2
Daniel Rodríguez <sadasant@gmail.com>
2012-09-18 00:16:00 -0430

    New faces.js for bootstrap purposes

--- _js/faces.js
+++ _js/faces.js
@@ -6,17 +6,14 @@ define('faces', [
   'jquery'
 , 'underscore'
 , 'text!../templates/face.html'
-, 'text!../templates/details.html'
-], function($, _, face_template, details_template) {
+], function($, _, face_template) {
 
-  var faces_target   // jQuery Object
-    , details_target // jQuery Object
+  var faces_target // jQuery Object
     , class_name   // String
     , callback     // Function
     , details = {}
 
   face_template = _.template(face_template)
-  details_template = _.template(details_template)
 
   function load(params, _callback) {
     var url        = '/json/members.json'
@@ -29,34 +26,24 @@ define('faces', [
   }
 
   function gotUsers(data) {
-    _.each(data, function(user, position, users) {
-      faces_target.append(face_template({
-        user : user
-      , class_name : class_name
-      }))
-      if (position === users.length - 1) {
-        faces_target.on('hover', '.' + class_name, showDetails)
-        if (callback) {
-          callback()
-        }
-      }
+    _.each(data, function(user) {
+      getDetails(user)
     })
   }
 
-  function showDetails() {
-    var login = $(this).attr('data-login')
-      ,  url
-    if (details[login]) {
-      details_target.html(details_template(details[login]))
-    } else {
-      url = '/json/users/' + login + '.json'
-      $.getJSON(url, function(data) {
-        details[login] = {
-          user : data
-        }
-        details_target.html(details_template(details[login]))
-      })
-    }
+  function getDetails(user) {
+    var url = '/json/users/' + user.login + '.json'
+    $.getJSON(url, function(details) {
+      writeFace(user, details)
+    })
+  }
+
+  function writeFace(user, details) {
+    faces_target.append(face_template({
+      user       : user
+    , class_name : class_name
+    , details    : details
+    }))
   }
 
   return {
