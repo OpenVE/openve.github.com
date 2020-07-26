f29fbadbc450f58ab1a73a457a0da1453e98e2ea
Esteban Borai <estebanborai@gmail.com>
2020-07-26 03:06:33 -0300

    add: about page with original content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,7 +18,7 @@ const Header = (): JSX.Element => (
         <NavLink to="about">Acerca</NavLink>
       </div>
       <div>
-        <a href="https://github.com/OpenVE" target="_blank" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+        <a href="https://github.com/OpenVE" target="_blank" rel="noopener noreferrer" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
           GitHub
         </a>
       </div>
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-function Home() {
+function Home(): JSX.Element {
   return (
     <div>
-      <h2>About</h2>
-      <code>Hello World!</code>
+      <h2>Acerca</h2>
+      <article>
+        <p>
+          OpenVE es una comunidad libre para la investigación y el desarrollo
+          de proyectos de computación de código abierto.
+        </p>
+        <p>
+          En OpenVE mantenemos tambien un listado de canales de Telegram, si tienes
+          interes en ver dicha lista, puedes hacer click aqui
+        </p>
+        <p>
+          Queremos impulsar el desarrollo de software de calidad en Venezuela, haciendo
+          disponible una biblioteca de código abierto, llena de conocimientos concebidos
+          por una comunidad no jerárquica de profesionales pertenecientes al sector público
+          y privado con años de experiencia, además de estudiantes entusiastas y otros
+          importantes colaboradores.
+        </p>
+      </article>
     </div>
   );
 }
