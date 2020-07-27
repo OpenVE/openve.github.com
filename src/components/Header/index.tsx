beb115b67c69c178159113c12e16f57b99f68534
Esteban Borai <estebanborai@gmail.com>
2020-07-26 22:38:27 -0300

    feat: open telegram group from header button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,14 +10,19 @@ const Header = (): JSX.Element => (
           <Logo />
         </NavLink>
       </div>
-      <div className="flex px-3 justify-center items-center">
+      <div className="flex justify-center items-center">
         <NavLink to="about">Acerca</NavLink>
       </div>
     </div>
     <div className="flex justify-center items-center">
-      <button className="block bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
+      <a
+        target="_blank"
+        href="https://t.me/openve"
+        rel="noopener noreferrer"
+        className="block bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded"
+      >
         Habla con Nosotros
-      </button>
+      </a>
     </div>
   </nav>
 );
