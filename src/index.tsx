7a042a63cb07480b4f7414238206b3af1771261e
Esteban Borai <estebanborai@gmail.com>
2020-07-30 15:36:34 -0300

    remove: service-workers dir

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Layout from './components/Layout';
-import * as serviceWorker from './service-workers/serviceWorker';
 
 import './tailwind.output.css';
 
@@ -12,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
